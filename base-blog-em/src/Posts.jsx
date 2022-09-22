084fb7bdb1abddfff3bd5aa68cdcafdfdfe37016
youngyoung0 <taeyoung6873@gmail.com>
2022-09-23 03:38:38 +0900

    stale time / cache time

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -17,7 +17,7 @@ export function Posts() {
 
   // replace with useQuery
   console.log(fetchPosts);
-  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts);
+  const { data, isError, error, isLoading } = useQuery("posts", fetchPosts, {staleTime: 2000});
   if (isLoading) return <h3>Loading...</h3>;
   /* isFetching / isLoading
   isFetching : 
