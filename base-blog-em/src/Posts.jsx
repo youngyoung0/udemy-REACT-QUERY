a97609895d06d0ae0c18db5329fb2835b1335724
youngyoung0 <taeyoung6873@gmail.com>
2022-09-26 09:17:27 +0900

    S2.데이터프리페칭(Pre-fetching)

--- base-blog-em/src/Posts.jsx
+++ base-blog-em/src/Posts.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { useQuery } from "react-query";
+import { useState, useEffect } from "react";
+import { useQuery, useQueryClient } from "react-query";
 
 import { PostDetail } from "./PostDetail";
 const maxPostPage = 10;
@@ -15,9 +15,17 @@ async function fetchPosts(pageNum) {
 export function Posts() {
   const [currentPage, setCurrentPage] = useState(0);
   const [selectedPost, setSelectedPost] = useState(null);
+  const queryClient = useQueryClient();
+
+  useEffect(() => {
+    if(currentPage < maxPostPage){
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(["posts", nextPage], () => fetchPosts(nextPage));
+    }
+  },[currentPage, queryClient]);
 
   const { data, isError, error, isLoading } =
-    useQuery(["posts", currentPage], () => fetchPosts(currentPage), { staleTime: 2000 });
+    useQuery(["posts", currentPage], () => fetchPosts(currentPage), { staleTime: 2000, keepPreviousData: true });
   /**
    * stale time / cache time
    * stale time
@@ -65,7 +73,9 @@ export function Posts() {
           Previous page
         </button>
         <span>Page {currentPage + 1}</span>
-        <button disabled={currentPage >= maxPostPage} onClick={() => { setCurrentPage((previousValue) => previousValue + 1) }}>
+        <button
+          disabled={currentPage >= maxPostPage}
+          onClick={() => { setCurrentPage((previousValue) => previousValue + 1) }}>
           Next page
         </button>
       </div>
